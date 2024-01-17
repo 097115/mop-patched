be89b030b2bd8ea1721bd9eb149367c33afd0b91
dsjkvf <dsjkvf@gmail.com>
2024-01-17 15:53:31 +0200

    perf: change time format

--- mop/screen.go
+++ mop/screen.go
@@ -134,7 +134,7 @@ func (screen *Screen) DrawOldMarket(market *Market) {
 func (screen *Screen) Draw(objects ...interface{}) *Screen {
 	zonename, _ := time.Now().In(time.Local).Zone()
 	if screen.pausedAt != nil {
-		defer screen.DrawLine(0, 0, `<right><r>`+screen.pausedAt.Format(`3:04:05pm `+zonename)+`</r></right>`)
+		defer screen.DrawLine(0, 0, `<right><r>`+screen.pausedAt.Format(`15:04:05 `+zonename)+`</r></right>`)
 	}
 	for _, ptr := range objects {
 		switch ptr.(type) {
@@ -145,7 +145,7 @@ func (screen *Screen) Draw(objects ...interface{}) *Screen {
 			object := ptr.(*Quotes)
 			screen.draw(screen.layout.Quotes(object.Fetch()), true)
 		case time.Time:
-			timestamp := ptr.(time.Time).Format(`3:04:05pm ` + zonename)
+			timestamp := ptr.(time.Time).Format(`15:04:05 ` + zonename)
 			screen.DrawLineInverted(0, 0, `<right><time>`+timestamp+`</></right>`)
 		default:
 			screen.draw(ptr.(string), false)
